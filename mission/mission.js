a599336a607e0ddc3c32808845c34ad83e831f8e
Thomas Meyers <funclemoz@gmail.com>
2026-01-17 19:01:21 -0700

    Added a dark theme to mission folder

---
+++ mission/mission.js
@@ -0,0 +1,28 @@
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current === 'dark') {
+        document.body.style.backgroundColor = '#333';
+        document.querySelector('h1').style.color = 'white';
+        document.querySelector('.subheader').style.color = '#9dbded'
+
+        document.querySelectorAll('p:not(.subheader), li').forEach(el => {
+            el.style.color = 'white';
+        });
+
+        logo.src = 'byui-logo-white.png';
+    } else if (current === 'light') { 
+        document.body.style.backgroundColor = 'white';
+        document.querySelector('h1').style.color = 'black';
+
+        document.querySelectorAll('p:not(.subheader), li').forEach(el => {
+            el.style.color = 'black';
+        });
+
+        logo.src = 'byui-logo-blue.png';
+    };
+}
