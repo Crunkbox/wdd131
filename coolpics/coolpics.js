167d3debb7aed6a40f03fd1528930758813dc1f3
Thomas Meyers <funclemoz@gmail.com>
2026-01-31 18:28:22 -0700

    Fixed menu and added Modal to img

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,17 +1,35 @@
 const menuLabel = document.getElementById("menu-label");
-const fullNavHTML = nav.innerHTML;
+const nav = document.querySelector("nav");
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
 
-function updateNav()
-{
-    if (window.innerWidth < 700) {
-        menuLabel.innerHTML = "Menu";
-        menuLabel.style.cursor = "pointer";
-    } else {
-        menuLabel.innerHTML = fullNavHTML;
-        menuLabel.style.cursor = "default";
-    }
+menuLabel.addEventListener("click", () => {
+    nav.classList.toggle("open");
+});
+
+gallery.addEventListener('click', openModal);
+
+function openModal (e) {
+    const image = e.target;
+    const src = image.getAttribute('src');
+    const alt = image.getAttribute('alt');
+    const largeSrc = src.replace('-sm', '-full');
+    modalImage.setAttribute('src', largeSrc);
+    modalImage.setAttribute('alt', alt);
+    modal.showModal();
+// Code to show modal  - Use event parameter 'e'   
+    
 }
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
 
-updateNav();
-
-window.addEventListener("resize", updateNav);
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
