28e988fd3bf67af74143dec4096fdce7dc690d62
Thomas Meyers <funclemoz@gmail.com>
2026-02-12 14:40:48 -0700

    Added stuff

--- w6_ponder/array.js
+++ w6_ponder/array.js
@@ -32,3 +32,53 @@ const studentHTML = students.map(student => {
 });
 
 studentContainer.innerHTML = studentHTML.join('');
+
+
+function convert(grade) {
+    switch (grade){
+        case 'A':
+            points = 4;
+            break;
+        case 'B':
+            points = 3;
+            break;
+        case 'C':
+            points = 2;
+            break;
+        case 'D':
+            points = 1;
+            break;
+        case 'F':
+            points = 0;
+            break;
+        default:
+            alert('not a valid grade');
+    }
+    return points;
+}
+          
+const grades = ['A', 'B', 'C', 'D'];
+
+const gradePoints = grades.map(convert);
+
+console.log(gradePoints);
+
+const GPAPoints = gradePoints.reduce((total, points) => total + points);
+console.log(GPAPoints);
+
+const gpa = GPAPoints / gradePoints.length;
+
+console.log(gpa)
+
+
+const words = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
+
+const filterFruits = words.filter((fruit) => {
+    return fruit.length >= 6;
+});
+
+console.log(filterFruits);
+
+words.forEach((fruit) => {
+    console.log(`This index is of ${fruit} is ${words.indexOf(fruit)}`);
+});
